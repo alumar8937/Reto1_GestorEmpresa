9ea449f1ee6976f97c0a7c92921906a7ae8602e5
pedro09pm <pedro09pm.principal@gmail.com>
2022-12-15 02:37:55 +0100

    Clase DocumentoCSV

---
+++ src/DocumentoCSV.java
@@ -0,0 +1,107 @@
+// Author: Pedro Marín Sanchis
+
+/*
+
+DocumentoCSV es una clase para el tratamiento de archivos CSV, como argumentos para el constructor toma
+Un File (archivo) y un String (delimitador). El propio constructor genera una estructura tal que:
+
+ArrayList de String arrays. Cada String array es un tuple del archivo CSV, includio el primero, que es
+la cabecera. Cada String de un String array es un valor en concreto del archivo CSV, su índice indica
+la columna en la que se encuentra.
+
+Por lo tanto podemos acceder a cualquier valor de un archivo CSV mediante dos valores enteros. Su pos-
+ición en la fila y su posición en la columna.
+
+ */
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DocumentoCSV {
+
+    final private String delimitador;
+    final private ArrayList<String[]> tuples;
+
+    public DocumentoCSV(File archivo, String delimitador) throws IOException {
+
+        this.delimitador = delimitador;
+        this.tuples = generarTuples(generarContenidoTextoPlano(archivo));
+
+    }
+
+    private String generarContenidoTextoPlano(File archivo) throws IOException { // Devuelve el contenido del fichero en un String.
+
+        BufferedReader reader = new BufferedReader(new FileReader(archivo));
+        StringBuilder builder = new StringBuilder();
+        String linea;
+
+        while ((linea = reader.readLine()) != null) {
+
+            builder.append(linea).append("\n"); // Realizamos append dos veces ya que estamos intentando evitar la concatenación reiterada de Strings. builder.append(linea + \n) es contraproducente.
+
+        }
+
+        reader.close();
+
+        return builder.toString();
+
+    }
+
+    private ArrayList<String[]> generarTuples(String contenidoTextoPlano) {
+
+        ArrayList<String[]> tuples = new ArrayList<>(1);
+
+        for (String i: contenidoTextoPlano.split("\n")) {
+
+            tuples.add(i.split(delimitador));
+
+        }
+
+        return tuples;
+
+    }
+
+    public String obtenerValor(int tuple, int columna) { // Devuelve el valor indicado (¡ LOS VALORES DE LA CABECERA DEL CSV SIGUEN ESTANDO !).
+
+        return tuples.get(tuple)[columna];
+
+    }
+
+    public void cambiarValor(int fila, int columna, String valor) { // Devuelve el valor indicado (¡ LOS VALORES DE LA CABECERA DEL CSV SIGUEN ESTANDO !).
+
+        String[] tupleModificado = tuples.get(fila);
+        tupleModificado[columna] = valor;
+        tuples.set(fila, tupleModificado);
+
+    }
+
+    public int obtenerCantidadDeCampos() { // Devuelve la cantidad de campos (¡ LA CABECERA DEL CSV SIGUE ESTANDO !).
+
+        return tuples.size();
+
+    }
+
+    public String obtenerContenidoTextoPlano() { // Devuelve el documento CSV como un String.
+
+        StringBuilder builder = new StringBuilder();
+
+        for (String[] i: tuples) {
+
+            for (String j: i) {
+
+                builder.append(j).append(delimitador);
+
+            }
+
+            builder.append("\n");
+
+        }
+
+        return builder.toString();
+
+    }
+
+}
--- src/GestorEmpresa.java
+++ src/GestorEmpresa.java
@@ -1,22 +1,45 @@
+// Author: Pedro Marín Sanchis
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
-
-// Author: Pedro Marín Sanchis
+import java.io.File;
 public class GestorEmpresa {
 
-    private static Scanner inputValue = new Scanner(System.in);
-    private static ArrayList<Departamento> departamentos = new ArrayList<Departamento>(1);
-    private static ArrayList<Empleado> empleados = new ArrayList<Empleado>(1);
+    final private static Scanner inputValue = new Scanner(System.in);
+    private static ArrayList<Departamento> departamentos = new ArrayList<>(1);
+    private static ArrayList<Empleado> empleados = new ArrayList<>(1);
 
     private static Boolean condicionDeSalida = false;
+
+    public static void probarClaseCSV() { // Author: Pedro Marín Sanchis
+
+        DocumentoCSV document = null;
+        try {
+            document = new DocumentoCSV(new File("CSV/Datos_Empresa.csv"), ";");
+            System.out.println(document.obtenerValor(4,5));
+            document.cambiarValor(4,5,"Jose");
+            System.out.println(document.obtenerValor(4,5));
+            System.out.println(document.obtenerContenidoTextoPlano());
+            exportarTexto(escogerArchivo(true), document.obtenerContenidoTextoPlano());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     public static void main(String[] args) { // Author: Raúl Simarro Navarro
 
         while (!condicionDeSalida) {
 
+            limpiarPantalla();
             mostrarMenu();
+            limpiarPantalla();
 
-        }
+       }
+
+        probarClaseCSV();
 
         inputValue.close();
 
@@ -26,21 +49,74 @@ public class GestorEmpresa {
         System.out.print("\n".repeat(50));
     }
 
-    private static void mostrarError(String textoError) { // Author: Raúl Simarro Navarro
+    private static void mostrarError(String mensaje) { // Author: Raúl Simarro Navarro
         limpiarPantalla();
-        System.out.print("[Error]: " + textoError + "\nPulsa ENTER para continuar...");
+        System.out.print("[Error]: " + mensaje + "\nPulsa ENTER para continuar...");
         inputValue.nextLine();
         limpiarPantalla();
     }
 
+    private static boolean mostrarPeticionDeConfirmacion(String mensaje) { // Author: Raúl Simarro Navarro, Pedro Marín Sanchis
+
+        while (true) {
+            limpiarPantalla();
+            System.out.print("[Aviso]: " + mensaje + "\n¿Deseas continuar? [Y/N]: ");
+            switch (inputValue.nextLine().toUpperCase()) {
+                case "Y": return true;
+                case "N": return false;
+            }
+            limpiarPantalla();
+        }
+
+    }
+
     private static void mostrarMenu() { // Author: Raúl Simarro Navarro
 
-        mostrarError("Prueba de error!");
         System.out.println("| 0.- Salir |1.-|2.-|3.-|4.-|5.-|");
         condicionDeSalida = true;
 
     }
 
+    private static File escogerArchivo(boolean avisarSiExiste) { // Author: Pedro Marín Sanchis
 
+        boolean entradaCorrecta = false;
+        File archivo = null;
+
+        while (!entradaCorrecta) {
+
+            limpiarPantalla();
+            System.out.print("Escribe la ruta del archivo: ");
+            archivo = new File(inputValue.nextLine());
+            if (archivo.exists() && avisarSiExiste) {
+
+                if (mostrarPeticionDeConfirmacion("Este archivo ya existe, en caso de continuar será reemplazado.")) {
+
+                    entradaCorrecta = true;
+
+                }
+
+            } else {
+
+                entradaCorrecta = true;
+
+            }
+
+        }
+
+        return archivo;
+
+    }
+
+    private static void exportarTexto(File destino, String contenido) { // Author: Pedro Marín Sanchis
+
+        try {
+            FileWriter writer = new FileWriter(destino);
+            writer.write(contenido);
+            writer.close();
+        } catch (IOException e) {
+            mostrarError("Error al exportar!");
+        }
+
+    }
 
 }
